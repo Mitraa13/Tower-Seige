59d1d165d5085b39855354cba2d4a4ad9e9b6388
Mitraa13 <mitraasrinivasan@gmail.com>
2020-12-01 18:53:26 +0530

    testing

--- Box.js
+++ Box.js
@@ -8,15 +8,26 @@ class box{
         this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
         this.height = height;
+        this.Visibility=255;
         World.add(world, this.body);
       }
       display(){
-        var angle = this.body.angle;
-        push();
-        translate(this.body.position.x, this.body.position.y);
-        rotate(angle);
-        rectMode(CENTER);
-        rect(0, 0, this.width, this.height);
-        pop();
-      }
+        if(this.body.speed<5){
+          var angle = this.body.angle;
+          push();
+          translate(this.body.position.x, this.body.position.y);
+          rotate(angle);
+          rectMode(CENTER);
+          rect(0, 0, this.width, this.height);
+          pop();
+        }
+        else {
+          World.remove(world,this.body);
+          push();
+          this.Visibility = this.Visibility-5;
+          tint(255,this.Visibility);
+          rect(this.body,this.body.position.x,this.body.position.y);
+          pop();
+        }
+     }
 }
